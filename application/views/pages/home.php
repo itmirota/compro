edfa548e9bf4ae616efca509d6aa5507dc378fa4
itmirota <itmirota@mirota.id>
2024-09-12 11:09:24 +0700

    penggantian banner

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -34,11 +34,11 @@
 <div id="carouselExampleInterval" class="carousel slide d-none d-lg-block" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="3000">
-        <img src="<?php echo base_url().'assets/landingpage/images/bannerlandscape1.webp'?>" class="d-block w-100 img-carousel">
+        <img src="<?php echo base_url().'assets/landingpage/images/l3.webp'?>" class="d-block w-100 img-carousel">
         </div>
-        <!--<div class="carousel-item" data-bs-interval="3000">-->
-        <!--<img src="<?php echo base_url().'assets/landingpage/images/banner-web2.webp'?>" class="d-block w-100" alt="...">-->
-        <!--</div>-->
+        <div class="carousel-item" data-bs-interval="3000">
+        <img src="<?php echo base_url().'assets/landingpage/images/skim.webp'?>" class="d-block w-100 img-carousel" alt="...">
+        </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
